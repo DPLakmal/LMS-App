cdcbe272942f9fa237df86c2caa83d5e7ef155c0
DPLakmal <pubudu.lakmalc@gmail.com>
2024-04-30 16:12:17 +0530

    refactor: Remove  testing file

--- app/(dashboard)/(routes)/student/courses/page.tsx
+++
@@ -1,9 +0,0 @@
-import Content from "@/app/(dashboard)/_components/content";
-
-const CoursesPage = () => {
-    return (<div className="md:my-4 ">
-        <Content />
-    </div>);
-}
-
-export default CoursesPage;
--- app/(dashboard)/(routes)/student/page.tsx
+++
@@ -1,5 +0,0 @@
-const StudentPagee = () => {
-    return (<div className="">studentpage</div>);
-}
-
-export default StudentPagee;
