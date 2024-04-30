3b4b78e50e23d0c2a9e40a44868cadc763e061be
DPLakmal <pubudu.lakmalc@gmail.com>
2024-04-30 22:45:09 +0530

    style: Add weekPage for subject page

--- app/(dashboard)/(routes)/(student)/courses/[...subjectCode]/page.tsx
+++ app/(dashboard)/(routes)/(student)/courses/[...subjectCode]/page.tsx
@@ -1,7 +1,13 @@
-const SubjectPage = () => {
+import WeekPage from "@/app/(dashboard)/_components/week";
+
+const SubjectPage = ({ params }: { params: { slug: string } }) => {
   return (
 
-    <div className="">SubjectPage</div>
+    <div className="w-full">
+      <div className="mt-6">
+        <WeekPage />
+      </div>
+    </div>
   );
 }
 
