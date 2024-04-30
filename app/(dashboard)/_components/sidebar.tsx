fdf5c9117dd6235daac565a8e8e8b6c1a198abd5
DPLakmal <pubudu.lakmalc@gmail.com>
2024-04-30 18:22:36 +0530

    style: Clean white space in code and fix css code

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,21 +4,17 @@ import { SideBarRoutes } from './sidebar-routes'
 export const SideBar = () => {
     return (
         <div className="h-full border-r flex flex-col 
-        overflow-y-auto bg-white shadow-sm">
-            <div className="p-6">
-                {/* <Logo /> */}
-            </div>
-            <div className="flex flex-col w-full">
+        overflow-y-auto bg-white shadow-sm mt-20">
+            <div className="flex flex-col w-full flex-1 justify-between">
                 <SideBarRoutes />
-
-                <div className="">
-                    <footer className="p-4 w-full bg-white border-t flex items-center justify-between">
-                        <div className="text-xs text-muted-foreground py-2 px-4 lg:flex-1">© 2024 ENRA d.o.o.</div>
-                        <button className="inline-flex items-center justify-center rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10 text-muted-foreground text-xs" type="button" id="radix-:R2tf8ta:" aria-haspopup="menu" aria-expanded="false" data-state="closed"></button>
-                    </footer>
-
+                <div className="p-4 w-full bg-white border-t flex items-center ">
+                    <div className="text-xs text-muted-foreground py-2 px-4 lg:flex-1">© 2024 LMS</div>
+                    <button className="inline-flex items-center justify-center rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10 text-muted-foreground text-xs" type="button" id="radix-:R2tf8ta:" aria-haspopup="menu" aria-expanded="false" data-state="closed">
+                        {/* ... icon  */}
+                    </button>
                 </div>
             </div>
-        </div>
+
+        </div >
     )
 }
