c4963f74cf0a124e2411d4301f9e6ccad77e69a2
DPLakmal <pubudu.lakmalc@gmail.com>
2024-04-30 16:11:19 +0530

    refactor: Extract to navbar to  _componentes/navbar.tsx

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,70 @@
+import { Button } from '@/components/ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuItem,
+} from '@/components/ui/dropdown-menu'
+import Image from 'next/image'
+import { Logo } from './logo'
+import { Input } from '@/components/ui/input'
+import { Search } from 'lucide-react'
+
+export const NavBar = () => {
+    return (
+        <div className="px-5 py-0 gap-x-4 flex-row flex w-full h-20 fixed items-center justify-between bg-white border-b z-[49] pl-6 ">
+            <div className="flex flex-row gap-5">
+                <Logo />
+                <div className="">
+                    <p className="font-semibold text-base text-sky-700">E - Learning Management System</p>
+                    <p className="text-xs text-muted-foreground">ATI - Galle</p>
+                </div>
+            </div>
+            <div className="ml-auto hidden lg:block">
+                <div className="flex items-center relative">
+                    <Input
+                        type="search"
+                        placeholder="Search for a course"
+                        className=" flex h-10 border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 w-full lg:w-[600px] rounded-lg rounded-r-none focus-visible:ring-transparent pr-8"
+                    />
+                    <Button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-sky-700 text-white hover:bg-sky-700/80 h-10 px-4 py-2 rounded-l-none">
+
+                        <Search className=" e left-2.5 top-2.5 h-4 w-4 " />
+                    </Button>
+                </div>
+
+            </div>
+
+
+            <div className="ml-auto">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="overflow-hidden rounded-full"
+                        >
+                            <Image
+                                src="https://avatar.iran.liara.run/public/boy?username=Ash"
+                                width={40}
+                                height={40}
+                                alt="Avatar"
+                                className="overflow-hidden rounded-full"
+                            />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Settings</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Logout</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
+        </div>
+    )
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,14 @@
+
 import { Logo } from './_components/logo'
 import MobileNavigationBar from './_components/mobile-navigationBar'
+import { NavBar } from './_components/navbar'
 import { SideBar } from './_components/sidebar'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full ">
             <div className="flex-1">
-                <div className="p-4 gap-x-4 flex-row flex w-full h-20 fixed items-center bg-yellow-300 border-b z-[49] pl-6 ">
-                    <Logo />
-                    <div className="">E - Learning Management System</div>
-                    <p>kdf</p>
-                </div>
-
+                <NavBar />
                 <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 -z-[48] mt-10">
                     <SideBar />
                 </div>
