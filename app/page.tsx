c96779da4f97a9a5f8d85e4229a925003a7477df
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-01 22:04:36 +0530

    feat: Rearrange Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,14 @@
-// import SideBar from './_components/(sidebar)/sideBar'
-// import Nav from './_components/(navbar)/nav'
-// import Content from './_components/(sidebar)/content'
-// import MobileNavigationBar from './_components/mobile-navigationBar'
-// import { getUserSession } from '@/lib/session'
-
-// export default async function Home() {
-//   const user = await getUserSession()
-//   return (
-//     <div className="flex flex-col h-screen w-screen">
-//       <Nav />
-//       <div className="flex-row flex">
-//         <div className="hidden md:flex ">
-//           <SideBar />
-//         </div>
-//         <div className="flex items-center justify-center w-full">
-//           {JSON.stringify(user)}
-//           <Content />
-//         </div>
-//       </div>
-//       <div className="flex md:hidden">
-//         <MobileNavigationBar />
-//       </div>
-//     </div>
-//   )
-// }
-
-
-import { getUserSession } from '@/lib/session'
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 
 export default async function Home() {
-  const user = await getUserSession()
-  return <main className="">{JSON.stringify(user)}</main>
+
+  return <main className="flex items-center justify-center h-screen">
+    <Link href={"/dashboard"}>
+      <Button size={'lg'}>
+        Go to LMS Dashboard
+      </Button>
+
+    </Link>
+  </main>
 }
