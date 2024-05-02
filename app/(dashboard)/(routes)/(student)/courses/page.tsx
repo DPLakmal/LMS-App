038f1c0c227b377a7ec79523ec1b5a76db1508d2
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-02 19:00:15 +0530

    feat: Add notice,pastpaper page files

--- app/(dashboard)/(routes)/(student)/courses/page.tsx
+++ app/(dashboard)/(routes)/(student)/courses/page.tsx
@@ -1,20 +1,23 @@
-import Content from "@/app/(dashboard)/_components/content";
+import Semesters from "@/app/(dashboard)/_components/semesters";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const CoursesPage = () => {
-    return (<div className="md:my-4 flex flex-col">
+    return (<div className="md:my-4 md:flex  md:flex-col flex-row  ">
         <div className="">
-            <Tabs defaultValue="Semester 1" className="w-full">
-                <TabsList>
+            <Tabs defaultValue="Semester 1" className="w-full ">
+                {/* <div className="fixed z-20 w-fit inset-x-0 top-20"> */}
+                <TabsList className="inline-flex">
                     <TabsTrigger value="Semester 1">Semester 1 </TabsTrigger>
                     <TabsTrigger value="Semester 2">Semester 2 </TabsTrigger>
                     <TabsTrigger value="Semester 3">Semester 3 </TabsTrigger>
                     <TabsTrigger value="Semester 4">Semester 4 </TabsTrigger>
                 </TabsList>
-                <TabsContent value="Semester 1"><Content /></TabsContent>
-                <TabsContent value="Semester 2"><Content /></TabsContent>
+                {/* </div> */}
+
+                <TabsContent value="Semester 1"><Semesters semester={1} /></TabsContent>
+                {/* <TabsContent value="Semester 2"><Content /></TabsContent>
                 <TabsContent value="Semester 3"><Content /></TabsContent>
-                <TabsContent value="Semester 4"><Content /></TabsContent>
+                <TabsContent value="Semester 4"><Content /></TabsContent> */}
             </Tabs>
 
         </div>
