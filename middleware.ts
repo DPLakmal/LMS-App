fc31786a22d638a2d000f9c0d8bb0abb1cb09773
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-12 21:38:48 +0530

    chore: Add page protection base on token role

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,34 @@
-import { NextResponse, NextRequest } from 'next/server'
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL('/home', request.url))
-}
+export default withAuth(function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token!.role);
+
+
+
+    if (req.nextUrl.pathname.startsWith("/lecturer") &&
+        req.nextauth.token!.role != "lecturer"
+    ) {
+        return NextResponse.rewrite(new URL("/Denided", req.url))
+    }
+
+
+    if (req.nextUrl.pathname.startsWith("/admin") &&
+        req.nextauth.token!.role != "admin"
+    ) {
+        return NextResponse.rewrite(new URL("/Denided", req.url))
+    }
+},
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token,
+        },
+    }
+);
+
+
 
 export const config = {
-    matcher: '/about/:path*',
+    matcher: ['/admin', '/lecturer/:path*'],
 }
