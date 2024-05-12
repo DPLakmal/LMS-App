2062547441ae781eeeb0bd600155950fabbc9c1a
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-12 21:37:51 +0530

    refactor: Make role based token

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,55 +1,69 @@
+import GoogleProvider from 'next-auth/providers/google'
+import dotenv from 'dotenv'
 import { prisma } from '@/lib/prisma'
 import { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import { profile } from 'console'
 
-export const options = {
+dotenv.config()
+
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       profile(profile) {
-        console.log(this.clientId)
-        console.log(this.clientSecret)
-
-        console.log(profile)
         return {
           id: profile.sub,
           name: profile.name,
           email: profile.email,
           image: profile.picture,
-          role: profile.role ?? 'user',
+          role: prisma.user.role,
         }
       },
     }),
   ],
 
   callbacks: {
+    async signIn({ profile, user }) {
+      const userData = {
+        email: profile.email,
+        username: profile.name,
+        role: '',
+      }
+
+      try {
+        const response = await fetch('http://localhost:3001/api/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        })
+
+        try {
+          const responseData = await response.json()
+          user.role = responseData.role
+          return true
+        } catch (jsonError) {
+          console.error('JSON parsing error:', jsonError.message)
+          return false
+        }
+
+        // prisma.user.role = 'fsdfs'
+      } catch (error) {
+        console.error('Registration error:', error.message)
+        // Handle registration error (display error message to the user, etc.)
+      }
+    },
+
     async jwt({ token, user }) {
       if (user) token.role = user.role
+      console.log(token)
       return token
     },
     async session({ session, token }) {
       if (session?.user) session.user.role = token.role
       return session
     },
-
-    // async signIn({ account, profile }) {
-    //   if (!profile?.email) {
-    //     throw new Error('no profile')
-    //   }
-
-    //   await prisma.user.upsert({
-    //     where: {
-    //       email: profile.email,
-    //     },
-    //     create: {
-    //       email: profile.email,
-    //       name: profile.name,
-    //     },
-    //     update: {
-    //       name: profile.name,
-    //     },
-    //   })
-    // },
   },
 }
