6c07fff00c5453b93981d31b8a4bbdbdd9026cd9
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-18 23:15:14 +0530

    refactor: Clean code

--- app/(dashboard)/(routes)/(student)/courses/page.tsx
+++ app/(dashboard)/(routes)/(student)/courses/page.tsx
@@ -1,32 +1,13 @@
-
-import Breadcrum from "@/app/(dashboard)/_components/breadcrumb";
 import SEM from "@/app/(dashboard)/_components/sem";
-import { usePathname } from "next/navigation";
+
 
 const CoursesPage = () => {
 
     return (<div className="md:my-4 md:flex  md:flex-col flex-row  ">
         <div className="">
-            {/* <Tabs defaultValue="Semester 1" className="w-full ">
-                 <div className="fixed z-20 w-fit inset-x-0 top-20">
-                <TabsList className="inline-flex">
-                    <TabsTrigger value="Semester 1">Semester 1 </TabsTrigger>
-                    <TabsTrigger value="Semester 2">Semester 2 </TabsTrigger>
-                    <TabsTrigger value="Semester 3">Semester 3 </TabsTrigger>
-                    <TabsTrigger value="Semester 4">Semester 4 </TabsTrigger>
-                </TabsList>
-                <TabsContent value="Semester 1"><Semesters semesterId={1} /></TabsContent>
-                <TabsContent value="Semester 2"><Semesters semesterId={2} /></TabsContent>
-                <TabsContent value="Semester 3"><Semesters semesterId={3} /></TabsContent>
-                <TabsContent value="Semester 4"><Semesters semesterId={4} /></TabsContent>
-            </Tabs>  */}
-
-
-            fdsdf
+            <div className="text-3xl text-center m-4">Semesters</div>
             <SEM></SEM>
         </div>
-
-
     </div >);
 }
 
