829904c4b6cb1665bacf91f026e79c395b73d1a1
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-18 23:36:01 +0530

    fix: Add key attribute

--- app/(dashboard)/_components/breadcrumb.tsx
+++ app/(dashboard)/_components/breadcrumb.tsx
@@ -4,22 +4,24 @@ import { Slash } from "lucide-react"
 
 
 const Breadcrum = ({ data }: any) => {
-    return (<div className="sdf">
+    return (<div className="inline-flex bg-slate-100 w-full p-2 rounded-xl">
 
 
-        <Breadcrumb>
-            <BreadcrumbList>
+        <Breadcrumb >
+            <BreadcrumbList >
                 {data.map((ref: any, index: any) => (
-                    <><BreadcrumbItem>
-                        <BreadcrumbLink href={`${ref.href}`}>{ref.Title}</BreadcrumbLink>
-                    </BreadcrumbItem>
+
+                    <div key={index} className=" flex flex-row justify-center items-center">
+
+                        <BreadcrumbItem className="hover:bg-blue-300 px-3 py-1 rounded-lg " >
+                            <BreadcrumbLink href={`${ref.href}`}>{ref.title}</BreadcrumbLink>
+                        </BreadcrumbItem>
 
                         <BreadcrumbSeparator className={(data.length - 1) == index ? ("hidden") : ("flex")}>
                             <Slash />
                         </BreadcrumbSeparator>
 
-
-                    </>
+                    </div>
                 ))}
 
             </BreadcrumbList>
