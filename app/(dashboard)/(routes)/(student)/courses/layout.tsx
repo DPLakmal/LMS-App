e9a3001c445cc94836dcc0cf5bc041cc3dcc25ae
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-18 23:14:53 +0530

    feact: Auto generate link path base on url

--- app/(dashboard)/(routes)/(student)/courses/layout.tsx
+++ app/(dashboard)/(routes)/(student)/courses/layout.tsx
@@ -5,25 +5,36 @@ import { usePathname } from "next/navigation"
 
 
 const CoursesPageLayout = ({ children }: { children: React.ReactNode }) => {
-    const pathname = usePathname()
-    return (
+    const path = usePathname()
+    // const path = "/courses/semester-1/1012";
 
+    // Split the path into parts
+    const parts = path.split('/');
+
+    // Initialize an empty array to store the objects
+    const resultArray: any = [];
+
+    // Iterate through the parts to construct the array of objects
+    parts.reduce((accumulator, currentPart, index) => {
+        if (currentPart !== "") {
+            const href = (index === 0 ? '/' : accumulator) + '/' + currentPart;
+            const title = currentPart === "" ? "" : currentPart.replace(/-/g, ' '); // Replace '-' with ' ' except for empty string
+            resultArray.push({ href, title });
+            return href;
+        } else {
+            return accumulator; // Skip the empty part
+        }
+    }, '');
+
+
+    return (
         <div className="">
             <div className="">
-                <div className="m-5">
-                    <Breadcrum data={[
-                        { href: `${pathname}`, Title: `${pathname.replaceAll(/\//g, '')}` },
-                        {
-                            href: "/",
-                            Title: "semester 1"
-                        }]
-                    }>
+                <div className="flex items-start m-5 ">
+                    <Breadcrum data={resultArray}>
                     </Breadcrum>
-
                 </div>
-
                 {children}
-
             </div>
         </div>
     );
