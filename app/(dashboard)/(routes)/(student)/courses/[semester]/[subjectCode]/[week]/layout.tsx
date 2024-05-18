733802f9cd4e5cf32f69089d3be5989aef8e4411
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-18 23:39:49 +0530

    feat: Add week Page layout

---
+++ app/(dashboard)/(routes)/(student)/courses/[semester]/[subjectCode]/[week]/layout.tsx
@@ -0,0 +1,12 @@
+const WeekPageLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+
+        <div className="">
+            week page layout
+
+            {children}
+        </div>
+    );
+}
+
+export default WeekPageLayout;
