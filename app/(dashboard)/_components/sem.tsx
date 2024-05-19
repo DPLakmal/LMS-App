1695ca2bb7a9b338b18fcc9980c143e03a483913
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-19 17:14:39 +0530

    style: Add some little style changement

--- app/(dashboard)/_components/sem.tsx
+++ app/(dashboard)/_components/sem.tsx
@@ -1,33 +1,27 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
+import SemesterPage from "../(routes)/(student)/courses/[semester]/page";
 
 const semesters = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
+    { id: 1, year: 1, title: "Semster 1" },
+    { id: 2, year: 1 },
+    { id: 3, year: 2 },
+    { id: 4, year: 2 },
 ];
 
 const SEM = () => {
     return (
-        <div className="grid gap-3 grid-cols-2">
+        <div className="grid gap-7 grid-cols-2">
             {semesters.map((semester, index) => (
                 <div key={index} className="semester-card">
                     <Link href={`courses/semester-${semester.id}`}>
-
                         <Card>
                             <CardHeader>
-                                <CardTitle>{`Semester ${semester.id}`}</CardTitle>
-                                {/* Add any relevant header content here */}
+                                <CardTitle className="text-2xl">{`Semester - ${semester.id}`}</CardTitle>
                             </CardHeader>
                             <CardContent>
-                                <p>This is content for Semester {semester.id}</p>
-                                {/* Add any relevant content here */}
+                                <p>Year {semester.year}</p>
                             </CardContent>
-                            <CardFooter>
-                                <p>Footer for Semester {semester.id}</p>
-                                {/* Add any relevant footer content here */}
-                            </CardFooter>
                         </Card>
                     </Link>
                 </div>
