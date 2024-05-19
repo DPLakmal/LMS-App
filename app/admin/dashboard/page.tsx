b41bbe031da1cb10924d53e5a1dbbdb57570f7c0
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-19 17:12:54 +0530

    feat: Add admin for data adding forms and display data

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,59 +1,7 @@
-
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable";
-
-import React from "react";
-import { Announcement } from "../_components/announcement"
-
-const AdminDashboard = () => {
-
-    // const [date, setDate] = React.useState<Date | undefined>(new Date())
-
-
+const Dashboard = () => {
     return (
-        <div className="w-full h-screen">
-            <ResizablePanelGroup direction="horizontal">
-                <ResizablePanel className="m-4 ">
-                    <Carousel>
-                        <CarouselContent>
-                            <CarouselItem><Card className="h-[300px]">
-                                <CardContent className="flex aspect-square items-center justify-center p-6">
-                                    <span className="text-4xl font-semibold">{ }</span>
-                                </CardContent>
-                            </Card></CarouselItem>
-                            <CarouselItem>...</CarouselItem>
-                            <CarouselItem>...</CarouselItem>
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-
-                </ResizablePanel>
-                <ResizableHandle withHandle />
-                <ResizablePanel defaultSize={30} className=" m-4">
-
-                    <ResizablePanelGroup direction="vertical">
-                        <ResizablePanel>
-                            <Announcement />
-                        </ResizablePanel >
-                        <ResizableHandle withHandle />
-                        <ResizablePanel defaultSize={50} className=" m-4">
-                            <Calendar
-                                mode="single"
-                                // selected={date}
-                                // onSelect={setDate}
-                                className="rounded-md border"
-                            />
-                        </ResizablePanel>
-                    </ResizablePanelGroup>
-                </ResizablePanel>
-            </ResizablePanelGroup>
-
-        </div>
+        <div className=""></div>
     );
 }
 
-export default AdminDashboard;
+export default Dashboard;
--- app/admin/lecturers/page.tsx
+++ app/admin/lecturers/page.tsx
@@ -1,91 +1,60 @@
-"use client"
-import React, { useState } from 'react';
-import axios from 'axios';
-import { DialogHeader, DialogFooter, DialogTitle, Dialog, DialogContent, DialogDescription, DialogTrigger } from '@/components/ui/dialog';
-import { Button } from '@/components/ui/button';
-import { toast } from '@/components/ui/use-toast';
-import { revalidatePath, revalidateTag } from 'next/cache';
-import * as DialogPrimitive from "@radix-ui/react-dialog"
 
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import AddLecturerButton from "../_components/AddLecturerButton";
+import DisplayLecturerList from "../_components/DisplayLecturerList";
+import { Separator } from "@/components/ui/separator";
+import Years from "../_components/years";
 
+const LecturerPage = () => {
+    return (<>
 
-function Form() {
-    const [formData, setFormData] = useState({
-        title: '',
-        description: '',
-    });
+        <div className="flex h-20">
 
-    const handleChange = (e: any) => {
-        console.log(e);
-
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('https://lms-app-backend-api-1.onrender.com/api/announcement', formData);
-            console.log('Form data submitted successfully:', response.data);
-            revalidatePath('/', 'page')
-        } catch (error) {
-            console.error('Error submitting form data:', error);
-        }
-
-    };
-
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="outline">Edit Profile</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-                <form onSubmit={handleSubmit}>
-                    <DialogHeader>
-                        <DialogTitle>Edit profile</DialogTitle>
-                        <DialogDescription>
-                            Make changes to your profile here. Click save when you're done.
-                        </DialogDescription>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                        <div className="grid grid-cols-4 items-center gap-4">
-
-                            <label>Title:</label>
-                            <input
-                                type="text"
-                                name="title"
-                                value={formData.title}
-                                onChange={handleChange} className="col-span-3" />
-
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <label>
-                                Description:
-                            </label>
-                            <input
-                                type="text"
-                                name="description"
-                                value={formData.description}
-                                onChange={handleChange} className="col-span-3" />
+            <div className="">
+                <div className="">
+                    <div className="md:my-4 md:flex  md:flex-col flex-row  ">
+                        <div className="">
+                            <Tabs defaultValue="Year 1">
+                                <div className="flex justify-center">
+                                    <TabsList className="grid grid-cols-2 h-12 w-[600px]">
+                                        <TabsTrigger className="py-2.5" value="Year 1">
+                                            Year 1
+                                        </TabsTrigger>
+                                        <TabsTrigger className="py-2.5" value="Year 2">
+                                            Year 2{' '}
+                                        </TabsTrigger>
+                                    </TabsList>
+                                </div>
+                                <TabsContent value="Year 1">
+                                    <Years YearId={1} />
+                                </TabsContent>
+                                <TabsContent value="Year 2">
+                                    <Years YearId={2} />
+                                </TabsContent>
+                            </Tabs>
                         </div>
                     </div>
-                    <DialogFooter>
-                        <Button type="submit" onClick={() => {
-                            toast({
-                                title: formData.title,
-                                description: formData.description,
-                            }),
+                </div>
 
-                                (DialogPrimitive.Close
-                                )
-                        }
+            </div>
+            <Separator orientation="vertical" className="mx-5" />
+            <div className="flex justify-between flex-col">
+                <div className="text-2xl m-6">Our Lecturer Pannel</div>
+                <div className="m-5">
+                    <DisplayLecturerList></DisplayLecturerList>
+                </div>
 
-                        }>Publish</Button>
-                    </DialogFooter>
-                </form>
-            </DialogContent>
-        </Dialog>
+
+
+                <AddLecturerButton />
+
+
+            </div>
+
+        </div>
+    </>
     );
 }
 
-export default Form;
+export default LecturerPage;
