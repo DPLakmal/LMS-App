f5ec94bdd6f62f791c680fae6284d4028534f1a3
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-21 19:58:46 +0530

    chore: Update rest api url and remove revaildation command

--- app/admin/_components/deleteAnnouncement.tsx
+++ app/admin/_components/deleteAnnouncement.tsx
@@ -1,14 +1,10 @@
-"use server"
-import { Button } from "@/components/ui/button";
+
 import axios from "axios";
-import { TrashIcon } from "lucide-react";
-import { revalidatePath, revalidateTag } from "next/cache";
 
 async function DeleteAnnouncement({ ItemID }: any) {
     try {
-        const response = await axios.delete(`http://localhost:3001/api/announcement/${ItemID}`);
+        const response = await axios.delete(`https://lms-ati-api.vercel.app/api/announcement?id=${ItemID}`);
         console.log('Announcement deleted successfully:', response.data);
-        revalidatePath('/admin/dashboard', 'layout')
 
     } catch (error) {
         console.error('Error deleting announcement:', error);
