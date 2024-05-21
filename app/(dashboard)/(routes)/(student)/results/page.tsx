98e7d3bb0711c9b911c4b07332ddfb2fc7340f68
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-21 19:44:24 +0530

    feat: Add Result page for dashboard student

---
+++ app/(dashboard)/(routes)/(student)/results/page.tsx
@@ -0,0 +1,11 @@
+import Result from "@/app/(dashboard)/_components/result";
+
+const ResultPage = () => {
+    return (
+        <div className="">
+            <Result></Result>
+        </div>
+    );
+}
+
+export default ResultPage;
