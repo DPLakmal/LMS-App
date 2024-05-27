9aaa6062c172efe74fc6a06a4a628363c88f5b05
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:45:39 +0530

    feact: Add sign in and sign up Ui button google

--- app/api/auth/signUp/SignUpComponent.tsx
+++ app/api/auth/signUp/SignUpComponent.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -8,7 +9,7 @@ import { useEffect } from "react";
 
 
 
-const SignInComponent = () => {
+export const SignUpComponent = () => {
 
 
     let redirectUrl = "http://location:3000";
@@ -26,6 +27,12 @@ const SignInComponent = () => {
     return (
 
         <>
+            <div className="m-1 flex justify-between">
+                <label htmlFor="" className="mr-3  w-fit inline-flex">Registration No</label>
+
+                <Input placeholder="GAL-IT-20XX-X-XXX" />
+            </div>
+
             <Button variant={"outline"} className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150" size={"lg"} onClick={handleClick}>
                 <Image height={24} width={24} className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
                 <span>Login with Google</span>
@@ -37,5 +44,3 @@ const SignInComponent = () => {
         </>
     );
 }
-
-export default SignInComponent;
