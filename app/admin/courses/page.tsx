b1a79dc371b03425188e90d2c4228354838aa905
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:48:50 +0530

    chore: Add all subject

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -9,42 +9,204 @@ import { useState } from "react";
 
 const CoursesAdminPage = () => {
 
-    // const subject = [
+    const subject = [{
+        code: '1012',
+        title: 'Visual Application Programming',
 
-    // ]
+    },
+    {
+        code: '1022',
+        title: 'Web Design',
 
-    const firstSemster = [
-        {
-            code: '1012',
-            title: 'Visual Application Programming',
+    },
+    {
+        code: '1032',
+        title: 'Computer and Network Systems',
 
-        },
-        {
-            code: '1022',
-            title: 'Web Design',
+    },
+    {
+        code: '1042',
+        title: 'Information Management and Information Systems',
 
-        },
-        {
-            code: '1032',
-            title: 'Computer and Network Systems',
+    },
+    {
+        code: '1052',
+        title: 'ICT Project (Individual)',
 
-        },
-        {
-            code: '1042',
-            title: 'Information Management and Information Systems',
+    },
+    {
+        code: '1062',
+        title: 'Communication Skills',
 
-        },
-        {
-            code: '1052',
-            title: 'ICT Project (Individual)',
+    },
+    {
+        code: '2012',
+        title: 'Fundamentals of Programming',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2022',
+        title: 'Software Development',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2032',
+        title: 'System Analysis and Design',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2042',
+        title: 'Data communication and Computer Networks',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2052',
+        title: 'Principles of User Interface Design',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2062',
+        title: 'ICT Project (Group)',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2072',
+        title: 'Technical Writing',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '2082',
+        title: 'Human Value and Professionalism',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3012',
+        title: 'Object Oriented Programming',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3022',
+        title: 'Web Programming',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3032',
+        title: 'Data Structures and Algorithms',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3042',
+        title: 'Database Management Systems',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3052',
+        title: 'Operating Systems',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3062',
+        title: 'Information and Computer Security',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '3072',
+        title: 'Statistics for IT',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4012',
+        title: 'Software Engineering',
+    },
+    {
+        code: '4022',
+        title: 'Software Quality Assurance',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4032',
+        title: 'IT Project Management',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4042',
+        title: 'Professional World',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4052',
+        title: 'Programming Individual Project',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4212',
+        title: 'Machine Learning',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4222',
+        title: 'Business Analysis Practice',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4232',
+        title: 'Enterprise Architecture',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
+    {
+        code: '4242',
+        title: 'Computer Services Management',
+        coverImg: '',
+        credit: 4,
+        subjects: [],
+    },
 
-        },
-        {
-            code: '1062',
-            title: 'Communication Skills',
-
-        },
     ]
+
 
     const weeks = [
         { "id": 1 },
@@ -65,9 +227,16 @@ const CoursesAdminPage = () => {
     ]
         ;
 
+    const [subjectCode, setSubjectCode] = useState("");
+    const [weekId, setWeekId] = useState("");
 
-    const [selectedSubject, setSelectedSubject] = useState("");
-    const [selectedWeek, setSelectedWeek] = useState("");
+    const handleSubjectChange = (value: string) => {
+        setSubjectCode(value);
+    };
+
+    const handleWeekChange = (value: string) => {
+        setWeekId(value);
+    };
 
     // // Your data for subjects and weeks
     // const firstSemester = [{ code: "CS101", title: "Computer Science 101" }, { code: "ENG101", title: "English 101" }];
@@ -77,13 +246,13 @@ const CoursesAdminPage = () => {
         <>
             <div className="w-full space-y-4">
                 <label htmlFor="" className="p-3">Select Subject :</label>
-                <Select>
+                <Select onValueChange={handleSubjectChange}>
                     <SelectTrigger className="w-[480px]">
                         <SelectValue placeholder="Select Subject" />
                     </SelectTrigger>
                     <SelectContent>
-                        {firstSemster.map((item: any) => (
-                            <SelectItem key={item.code} value={item.code} onSelect={() => setSelectedSubject(item.code)}>
+                        {subject.map((item: any) => (
+                            <SelectItem key={item.code} value={item.code} >
                                 {item.code} - {item.title}
                             </SelectItem>
                         ))}
@@ -91,20 +260,20 @@ const CoursesAdminPage = () => {
                 </Select>
 
                 <label htmlFor="" className="p-3">Select Week :</label>
-                <Select>
+                <Select onValueChange={handleWeekChange}>
                     <SelectTrigger className="w-[180px]">
                         <SelectValue placeholder="Select Week No" />
                     </SelectTrigger>
                     <SelectContent>
                         {weeks.map((item: any) => (
-                            <SelectItem key={item.id} value={item.id} onSelect={() => setSelectedWeek(item.id)}>
+                            <SelectItem key={item.id} value={item.id} >
                                 Week - {item.id}
                             </SelectItem>
                         ))}
                     </SelectContent>
                 </Select>
 
-                <InputForm subjectCode={selectedSubject} weekId={selectedWeek} />
+                <InputForm subjectCode={subjectCode} weekId={weekId} />
 
             </div>
         </>
