d72f5bf2adc67f44eb07b0af30d955b3e65e9cf9
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:41:57 +0530

    feact: Add new lecturerAnnoucement with form

--- app/(dashboard)/(routes)/lecturer/dashboard/page.tsx
+++ app/(dashboard)/(routes)/lecturer/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/componen
 import { Check } from "lucide-react";
 import React from "react";
 
-
+import { LecturerAnnouncement } from "@/app/admin/_components/announcement-traget/announcement-Lecturer";
 const LecturerDashboard = () => {
 
     const [date, setDate] = React.useState<Date | undefined>(new Date())
@@ -51,38 +51,7 @@ const LecturerDashboard = () => {
 
                     <ResizablePanelGroup direction="vertical">
                         <ResizablePanel>
-                            <Card className="w-fit">
-                                <CardHeader>
-                                    <CardTitle>Notifications</CardTitle>
-                                    <CardDescription>You have 3 unread messages.</CardDescription>
-                                </CardHeader>
-                                <CardContent className="grid gap-4">
-
-                                    <div>
-                                        {notifications.map((notification, index) => (
-                                            <div
-                                                key={index}
-                                                className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                            >
-                                                <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                                <div className="space-y-1">
-                                                    <p className="text-sm font-medium leading-none">
-                                                        {notification.title}
-                                                    </p>
-                                                    <p className="text-sm text-muted-foreground">
-                                                        {notification.description}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
-                                </CardContent>
-                                <CardFooter>
-                                    <Button className="w-full">
-                                        <Check className="mr-2 h-4 w-4" /> Mark all as read
-                                    </Button>
-                                </CardFooter>
-                            </Card>
+                            <LecturerAnnouncement />
                         </ResizablePanel >
                         <ResizableHandle withHandle />
                         <ResizablePanel defaultSize={50} className=" m-4">
