afb5a5fdf98753f6af6a04e8ac56e6ee7fe32af6
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:52:31 +0530

    chore: Add react pdf

---
+++ app/doc/page.tsx
@@ -0,0 +1,14 @@
+
+import { Document, Page } from 'react-pdf'
+const sdfsdf = () => {
+    return (<div className="
+    ">
+        <Document file="https://firebasestorage.googleapis.com/v0/b/lms-app-c508c.appspot.com/o/4012%2FWeek1-Introduction%20to%20SE.pdf?alt=media&token=f1e421f4-5ee8-43fd-9aa6-7d055491a3be" >
+            <Page pageNumber={1}></Page>
+        </Document>
+
+
+    </div>);
+}
+
+export default sdfsdf;
