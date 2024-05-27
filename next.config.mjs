ebeb028ffc54bd4984f6a588a5020a433e9894b8
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:34:26 +0530

    chore: Add file loader

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,16 @@ const nextConfig = {
       'foundr.com',
       'avatar.iran.liara.run',
       'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
     ],
+  },
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle .node files
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'file-loader',
+    })
+    return config
   },
 }
 
