29a86fecce8ae25141eae7e0220c141fce322825
DPLakmal <pubudu.lakmalc@gmail.com>
2024-05-27 22:44:19 +0530

    refactor: Remove that files

--- app/(student)/(auth)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "../_ui/registration-form";
-
-const studentRegisterPage = () => {
-    return (<>
-        <RegisterForm />
-    </>);
-}
-
-export default studentRegisterPage;
